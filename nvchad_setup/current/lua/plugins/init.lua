2e1526d729b9e3328a3cbd370651aa65840ab7fc
Abdullah Abdulwahab <a77x86@gmail.com>
2025-09-27 20:19:47 +0400

    feat: Simplify LSP setup with direct npm installation
    - Removed mason-lspconfig to avoid version conflicts
    - Installed pyright and typescript-language-server globally via npm
    - Direct LSP configuration without auto-installation complexity
    - Both Python and TypeScript LSPs working and tested
    - Standard, reliable setup for Neovim 0.10.1

--- nvchad_setup/current/lua/plugins/init.lua
+++ nvchad_setup/current/lua/plugins/init.lua
@@ -29,22 +29,7 @@ return {
   },
 
   {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
-    config = function()
-      require("mason-lspconfig").setup()
-      require("mason-lspconfig").setup_handlers({
-        -- Default handler for all servers
-        function(server_name)
-          require("lspconfig")[server_name].setup({})
-        end,
-      })
-    end,
-  },
-
-  {
     "neovim/nvim-lspconfig",
-    dependencies = { "williamboman/mason-lspconfig.nvim" },
     config = function()
       require "configs.lspconfig"
     end,
